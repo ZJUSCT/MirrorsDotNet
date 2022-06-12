7ab3bfb70e0c39ced1675de1e8123d0d5f0a3cf6
NCJ Charlie <me@ncj.wiki>
2022-06-12 11:04:13 +0800

    db: change i18n to single column

--- Manager/Models/MirrorContext.cs
+++ Manager/Models/MirrorContext.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace Manager.Models;
@@ -19,6 +18,14 @@ public class MirrorContext : DbContext
         modelBuilder.Entity<MirrorSyncJob>()
             .HasIndex(b => b.Status)
             .HasFilter("[Status] < 3");
+        modelBuilder.Entity<MirrorItem>().Property(p => p.Name).HasConversion(
+            v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
+            v => JsonSerializer.Deserialize<I18N.StringBase>(v, (JsonSerializerOptions)default)
+        );
+        modelBuilder.Entity<MirrorItem>().Property(p => p.Description).HasConversion(
+            v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
+            v => JsonSerializer.Deserialize<I18N.StringBase>(v, (JsonSerializerOptions)default)
+        );
         modelBuilder.Entity<MirrorItem>().Property(p => p.Container).HasConversion(
             v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
             v => JsonSerializer.Deserialize<Container>(v, (JsonSerializerOptions)default)
@@ -27,35 +34,5 @@ public class MirrorContext : DbContext
             v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
             v => JsonSerializer.Deserialize<Container>(v, (JsonSerializerOptions)default)
         );
-        // modelBuilder.Entity<MirrorItem>().OwnsOne(
-        //     p => p.Container, od =>
-        //     {
-        //         od.Property(p => p.Volumes)
-        //             .HasConversion(
-        //                 v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
-        //                 v => JsonSerializer.Deserialize<List<Volume>>(v, (JsonSerializerOptions)default)
-        //             );
-        //         od.Property(p => p.Environments)
-        //             .HasConversion(
-        //                 v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
-        //                 v => JsonSerializer.Deserialize<List<Environment>>(v, (JsonSerializerOptions)default)
-        //             );
-        //     }
-        // );
-        // modelBuilder.Entity<MirrorSyncJob>().OwnsOne(
-        //     p => p.Container, od =>
-        //     {
-        //         od.Property(p => p.Volumes)
-        //             .HasConversion(
-        //                 v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
-        //                 v => JsonSerializer.Deserialize<List<Volume>>(v, (JsonSerializerOptions)default)
-        //             );
-        //         od.Property(p => p.Environments)
-        //             .HasConversion(
-        //                 v => JsonSerializer.Serialize(v, (JsonSerializerOptions)default),
-        //                 v => JsonSerializer.Deserialize<List<Environment>>(v, (JsonSerializerOptions)default)
-        //             );
-        //     }
-        // );
     }
 }
