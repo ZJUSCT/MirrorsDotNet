c8bb00d7e3074f4e68e72469fbe3717866f1a351
Azuk 443 <me@azuk.top>
2024-05-03 16:57:47 +0800

    Add MirrorZ api

---
+++ Orchestrator/Controllers/MirrorZ.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orchestrator.DataModels;
+using Orchestrator.Services;
+using Orchestrator.Utils;
+
+namespace Orchestrator.Controllers;
+
+[ApiController]
+[Route("/mirrorz.json")]
+[Produces("application/json")]
+public class MirrorZInfo(IConfiguration conf, ILogger<MirrorZInfo> log, JobQueue jobQueue) : CustomControllerBase(conf)
+{
+    private string TransformStatus(SyncJob job)
+    {
+        var item = job.MirrorItem;
+        if (item.Status == MirrorStatus.Cached) return "C";
+        if (item.Status == MirrorStatus.Succeeded)
+            return $"S{item.LastSuccessAt.ToUnixTimeSeconds()}X{job.TaskShouldStartAt.ToUnixTimeSeconds()}";
+        if (item.Status == MirrorStatus.Syncing)
+            return $"Y{job.TaskStartedAt.ToUnixTimeSeconds()}O{item.LastSuccessAt.ToUnixTimeSeconds()}";
+        if (item.Status == MirrorStatus.Failed)
+            return $"F{item.LastSyncAt.ToUnixTimeSeconds()}O{item.LastSuccessAt.ToUnixTimeSeconds()}";
+        return "U";
+    }
+    
+    [HttpGet("")]
+    public ActionResult<MirrorZData> GetMirrorZData()
+    {
+        var (pendingJobs, syncingJobs) = jobQueue.GetJobs();
+        var jobs = (pendingJobs as IEnumerable<SyncJob>).Concat(syncingJobs).Where(x => x.Stale == false).ToList();
+        var transformedItems = jobs
+            .Select(x => new MirrorZMirrorItem(
+                x.MirrorItem.Config.Id,
+                x.MirrorItem.Config.Info.Description.Zh,
+                x.MirrorItem.Config.Info.Url,
+                TransformStatus(x),
+                $"/docs/{x.MirrorItem.Config.Id}",
+                x.MirrorItem.Config.Info.Upstream,
+                "0"
+            )).ToList();
+
+        return new MirrorZData(
+            1.7,
+            MirrorZStatic.SiteInfo,
+            new List<MirrorZCatItem>(),
+            transformedItems);
+    }
+}
---
+++ Orchestrator/DataModels/MirrorZ.cs
@@ -0,0 +1,56 @@
+﻿namespace Orchestrator.DataModels;
+
+public record MirrorZData(
+    double version,
+    MirrorZSiteInfo site,
+    IList<MirrorZCatItem> info,
+    IList<MirrorZMirrorItem> mirrors);
+
+public record MirrorZSiteInfo(
+    string url,
+    string? logo,
+    string? logo_darkmode,
+    string? abbr,
+    string? name,
+    string? homepage,
+    string? issue,
+    string? request,
+    string? email,
+    string? group,
+    string? disk,
+    string? note,
+    string? big,
+    bool disable = false);
+
+public record MirrorZCatItemUrl(string name, string url);
+
+public record MirrorZCatItem(string distro, string category, IList<MirrorZCatItemUrl> urls);
+
+public record MirrorZMirrorItem(
+    string cname,
+    string desc,
+    string url,
+    string status,
+    string help,
+    string upstream,
+    string size,
+    bool disable = false);
+
+public static class MirrorZStatic
+{
+    public static MirrorZSiteInfo SiteInfo = new MirrorZSiteInfo(
+        "https://mirrors.zju.edu.cn",
+        "https://mirrors.zju.edu.cn/zju-falcon.svg",
+        null,
+        "ZJU",
+        "浙江大学开源软件镜像站",
+        "https://www.zjusct.io",
+        null,
+        null,
+        "mirrors@zju.edu.cn",
+        null,
+        null,
+        null,
+        null
+    );
+}
